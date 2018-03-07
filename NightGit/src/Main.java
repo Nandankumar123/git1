a0f56c93b3ddb5f8f200ecf011ce625f8193e14e
Nandankumar123 <36264506+Nandankumar123@users.noreply.github.com>
2018-03-07 23:12:20 +0530

    Update Main.java
    changed

--- NightGit/src/Main.java
+++ NightGit/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Goood night");
+		System.out.println("Goood night sweet drems");
 	}
 
 }
